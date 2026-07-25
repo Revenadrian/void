cf742d549cea0b89ea17246598f9883ec00f82af
Revenadrian <revenu24@gmail.com>
2026-07-25 12:51:55 +0700

    Update 10k average pace in script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const config = {
 
     pb: {
          "5k": "Avg Pace 6:24 00:31:59",
-        "10k": "Avg Pace 7:30 01:15:00",
+        "10k": "Avg Pace 7:15 01:12:45",
         "15k": "Avg Pace 7:36 01:54:03",
         "21k": "Avg Pace 7:45 02:43:20"
     },
